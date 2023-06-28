8088fb753332a7d12387bc32cc8709a5175b1305
Martin <Ponchogmartin@hotmail.com>
2023-06-28 14:04:00 -0500

    login agregado

---
+++ Grupo8_Autoshop/Login.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Grupo8_Autoshop
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.bTNACCEDER = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtaceptar = new System.Windows.Forms.TextBox();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bTNACCEDER
+            // 
+            this.bTNACCEDER.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bTNACCEDER.Location = new System.Drawing.Point(40, 379);
+            this.bTNACCEDER.Name = "bTNACCEDER";
+            this.bTNACCEDER.Size = new System.Drawing.Size(176, 43);
+            this.bTNACCEDER.TabIndex = 0;
+            this.bTNACCEDER.Text = "ACCEDER";
+            this.bTNACCEDER.UseVisualStyleBackColor = true;
+            this.bTNACCEDER.Click += new System.EventHandler(this.bTNACCEDER_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(287, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SALIR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "AUTOSHOP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "USUARIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CONTRASEÑA";
+            // 
+            // txtaceptar
+            // 
+            this.txtaceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtaceptar.Location = new System.Drawing.Point(137, 172);
+            this.txtaceptar.Name = "txtaceptar";
+            this.txtaceptar.Size = new System.Drawing.Size(253, 30);
+            this.txtaceptar.TabIndex = 5;
+            // 
+            // txtcontraseña
+            // 
+            this.txtcontraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcontraseña.Location = new System.Drawing.Point(137, 235);
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.PasswordChar = '*';
+            this.txtcontraseña.Size = new System.Drawing.Size(253, 34);
+            this.txtcontraseña.TabIndex = 6;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(501, 494);
+            this.Controls.Add(this.txtcontraseña);
+            this.Controls.Add(this.txtaceptar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bTNACCEDER);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bTNACCEDER;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtaceptar;
+        private System.Windows.Forms.TextBox txtcontraseña;
+    }
+}
---
+++ Grupo8_Autoshop/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grupo8_Autoshop
+{
+    public partial class Login : Form
+    {
+        string Usuario = "Administrador";
+        string Contraseña = "Hola";
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void bTNACCEDER_Click(object sender, EventArgs e)
+        {
+            if(txtaceptar.Text != Usuario && txtcontraseña.Text != Contraseña)
+            {
+                if(txtaceptar.Text != Usuario)
+                {
+                    MessageBox.Show("usuario incorrecto");
+                    txtaceptar.Clear();
+                    txtaceptar.Focus();
+                    return;
+                       
+                }if(txtcontraseña.Text != Contraseña)
+                {
+                    MessageBox.Show("contraseña incorrecto");
+                    txtcontraseña.Clear();
+                    txtcontraseña.Focus();
+                    return;
+                }
+
+            }
+            else
+            {
+                txtaceptar.Clear();
+                txtcontraseña.Clear();
+                Form1 f = new Form1();
+                f.ShowDialog();
+            }
+        }
+    }
+}
